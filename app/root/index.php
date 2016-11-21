27d6cd260b7da559df77170bc03714b9f7e0c437
monkick <saruyama.monki@gmail.com>
2016-11-21 14:06:21 +0900

    gulp + webpack + php + bootstrap

---
+++ app/root/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>index</title>
+
+    <link rel="stylesheet" href="asset/css/index.css">
+
+    <script src="/asset/js/bundle.js" async></script>
+</head>
+<body>
+
+<?= "Hello PHP" ?>
+
+<div class="container">
+    <button class="btn btn-danger">a</button>
+</div>
+
+
+</body>
+</html>
---
+++ app/src/js/main.js
@@ -0,0 +1,5 @@
+window.$ = window.jQuery = require('jquery');
+require('bootstrap-sass');
+
+require('../sass/index.scss');
+require('../js/index.js');
